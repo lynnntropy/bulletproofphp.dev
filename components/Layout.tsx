c36400b5210cb01b9c806a171b1365c5755772e0
Veselin Romić <omegavesko@gmail.com>
2021-10-30 00:14:50 +0200

    feat: newsletter banner on homepage

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 const Layout: React.FC = ({ children }) => (
   <div className="min-h-screen flex flex-col bg-gray-50">
     <Header />
-    <main className="flex-1">{children}</main>
+    <main className="flex-1 flex flex-col">{children}</main>
     <Footer />
   </div>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,24 +6,28 @@ import matter from "gray-matter";
 import Link from "next/link";
 import { postPath } from "../utils/path";
 import { format as formatDate, parseISO as parseISODate } from "date-fns";
+import NewsletterBanner from "../components/NewsletterBanner";
 
 type Props = ReturnType<typeof getStaticProps>["props"];
 
 const Home: NextPage<Props> = ({ posts }) => {
   return (
-    <div className="max-w-2xl w-full mx-auto flex flex-col gap-4">
-      {posts.map((post) => (
-        <Link key={post.slug} href={postPath(post.slug)}>
-          <a>
-            <div className="flex items-center bg-white shadow rounded px-5 py-3">
-              <span className="flex-1">{post.data.title}</span>
-              <span className="text-sm text-gray-500">
-                {formatDate(parseISODate(post.data.date), "MMM do, yyyy")}
-              </span>
-            </div>
-          </a>
-        </Link>
-      ))}
+    <div className="flex-1 flex flex-col">
+      <div className="flex-1 mb-24 max-w-2xl w-full mx-auto flex flex-col gap-4">
+        {posts.map((post) => (
+          <Link key={post.slug} href={postPath(post.slug)}>
+            <a>
+              <div className="flex items-center bg-white shadow rounded px-5 py-3">
+                <span className="flex-1">{post.data.title}</span>
+                <span className="text-sm text-gray-500">
+                  {formatDate(parseISODate(post.data.date), "MMM do, yyyy")}
+                </span>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </div>
+      <NewsletterBanner />
     </div>
   );
 };
