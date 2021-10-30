8220617082c185defac0739b04cc99bab07eb190
Veselin Romić <omegavesko@gmail.com>
2021-10-30 16:41:13 +0200

    fix: proper header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
-import Logo from "../assets/images/logo.svg";
 import Link from "next/link";
 import { homePath } from "../utils/path";
+import Twemoji from "./Twemoji";
 
 const Header: React.FC = () => (
   <div className="h-20 mb-8 flex justify-center items-center lg:mb-24">
     <Link href={homePath()}>
       <a>
-        <Logo className="h-8 w-auto sm:h-10" alt="Bulletproof PHP" />
+        <span className="font-title text-xl font-bold lg:text-2xl">
+          <Twemoji emoji="🐘" /> Bulletproof PHP
+        </span>
       </a>
     </Link>
   </div>
