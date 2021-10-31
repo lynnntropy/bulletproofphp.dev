91d262cd8e9298d4a57d640771c8b910f56e8c41
Veselin Romić <omegavesko@gmail.com>
2021-10-31 12:03:18 +0100

    fix: actually sort posts on the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,11 @@ import fs from "fs";
 import matter from "gray-matter";
 import Link from "next/link";
 import { postPath } from "../utils/path";
-import { format as formatDate, parseISO as parseISODate } from "date-fns";
+import {
+  format as formatDate,
+  parseISO as parseISODate,
+  compareDesc as compareDatesDesc,
+} from "date-fns";
 import NewsletterBanner from "../components/NewsletterBanner";
 
 type Props = ReturnType<typeof getStaticProps>["props"];
@@ -15,18 +19,27 @@ const Home: NextPage<Props> = ({ posts }) => {
     <>
       <div className="flex-1 flex flex-col">
         <div className="flex-1 mb-8 max-w-2xl w-full mx-auto flex flex-col gap-4 px-3 md:px-0 sm:mb-24">
-          {posts.map((post) => (
-            <Link key={post.slug} href={postPath(post.slug)}>
-              <a>
-                <div className="flex flex-col items-start bg-white shadow rounded px-5 py-3 md:flex-row md:items-center">
-                  <span className="flex-1 font-medium">{post.data.title}</span>
-                  <span className="text-sm text-gray-500">
-                    {formatDate(parseISODate(post.data.date), "MMM do, yyyy")}
-                  </span>
-                </div>
-              </a>
-            </Link>
-          ))}
+          {posts
+            .sort((a, b) =>
+              compareDatesDesc(
+                parseISODate(a.data.date),
+                parseISODate(b.data.date)
+              )
+            )
+            .map((post) => (
+              <Link key={post.slug} href={postPath(post.slug)}>
+                <a>
+                  <div className="flex flex-col items-start bg-white shadow rounded px-5 py-3 md:flex-row md:items-center">
+                    <span className="flex-1 font-medium">
+                      {post.data.title}
+                    </span>
+                    <span className="text-sm text-gray-500">
+                      {formatDate(parseISODate(post.data.date), "MMM do, yyyy")}
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            ))}
         </div>
         <NewsletterBanner />
       </div>
